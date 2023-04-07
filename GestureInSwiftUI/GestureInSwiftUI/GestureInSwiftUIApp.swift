a5ffee00b1a1278ba50575c0a88d13cb133b6002
Donguk Kim <ehddnr73@naver.com>
2023-04-07 19:00:28 +0900

    GestureInSwiftUI 프로젝트 생성

---
+++ GestureInSwiftUI/GestureInSwiftUI/GestureInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureInSwiftUIApp.swift
+//  GestureInSwiftUI
+//
+//  Created by 김동욱 on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct GestureInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
