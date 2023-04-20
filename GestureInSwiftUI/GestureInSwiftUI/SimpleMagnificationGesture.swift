13af3c94df6eca60108af95f698f91d515fa1490
Donguk Kim <ehddnr73@naver.com>
2023-04-20 15:08:50 +0900

    .onEnded 적용

--- GestureInSwiftUI/GestureInSwiftUI/SimpleMagnificationGesture.swift
+++ GestureInSwiftUI/GestureInSwiftUI/SimpleMagnificationGesture.swift
@@ -1,5 +1,5 @@
 //
-//  SimpleMagnificationGesture.swift
+//  MyMagnificationGesture.swift
 //  GestureInSwiftUI
 //
 //  Created by 김동욱 on 2023/04/20.
@@ -9,23 +9,28 @@ import SwiftUI
 
 struct SimpleMagnificationGesture: View {
     @State var currentAmount: CGFloat = 0
+    @State var lastAmount: CGFloat = 0
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .font(.title)
             .padding(40)
             .background(Color.red.cornerRadius(10))
-            .scaleEffect(1 + currentAmount)
+            .scaleEffect(1 + currentAmount + lastAmount)
             .gesture(
                 MagnificationGesture()
                     .onChanged { value in
                         currentAmount = value - 1
                     }
+                    .onEnded { value in
+                        lastAmount += currentAmount
+                        currentAmount = 0
+                    }
             )
     }
 }
 
-struct MyMagnificationGesture_Previews: PreviewProvider {
+struct SimpleMagnificationGesture_Previews: PreviewProvider {
     static var previews: some View {
         SimpleMagnificationGesture()
     }
